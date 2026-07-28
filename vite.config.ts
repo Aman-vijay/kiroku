1c79ce171a2c6ca5afffadf5d684e14f4fdab7ad
Aman-vijay <shivamvj04@gmail.com>
2026-07-28 23:45:02 +0530

    Updated vercel config:hopefully works 😭😭😭

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
-    devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
     tanstackStart(),
-    nitroV2Plugin({
-      preset: 'vercel',
-      compatibilityDate: '2026-07-18',
-    }),
+    nitro(),
+    devtools(),
+    tailwindcss(),
     viteReact(),
   ],
 })
